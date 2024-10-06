414661324a475609848349663fb4d1d6b6a2c680
CLopMan <cesarlopman@gmail.com>
2024-10-06 13:30:49 +0200

    error in bibliography

---
+++ Plantilla_TFG_2019/cesar.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+pdflatex plantilla_tfg_2019.tex
+biber plantilla_tfg_2019
+pdflatex plantilla_tfg_2019.tex
+for i in $(ls | grep plantilla | grep -v tex | grep -v xmpdata | grep -v pdf); do rm -f $i; done
+mkdir -p build
+mv plantilla_tfg_2019.pdf build/report.pdf
