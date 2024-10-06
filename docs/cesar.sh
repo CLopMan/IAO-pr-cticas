83d362ed41dcdf2e1ef33e27d48128fe0252c73a
CLopMan <cesarlopman@gmail.com>
2024-10-06 23:44:21 +0200

    change folder name and update self script

--- docs/cesar.sh
+++ docs/cesar.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+if [ $# -eq 0 ]; then
+    n=3
+else 
+    n=$1
+fi
 
-biber plantilla_tfg_2019
-pdflatex plantilla_tfg_2019.tex
-biber plantilla_tfg_2019
-pdflatex plantilla_tfg_2019.tex
-pdflatex plantilla_tfg_2019.tex
-pdflatex plantilla_tfg_2019.tex
-pdflatex plantilla_tfg_2019.tex
+printf "number of compilations = $n\n"
 
+pdflatex plantilla_tfg_2019.tex
+biber plantilla_tfg_2019
+for i in $(seq 0 $n); do pdflatex plantilla_tfg_2019.tex; done
+
+# delete aux files
 for i in $(ls | grep plantilla | grep -v tex | grep -v xmpdata | grep -v pdf); do rm -f $i; done
+
 mkdir -p build
 mv plantilla_tfg_2019.pdf build/report.pdf
